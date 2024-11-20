5815e76f1aa2b1aa928cc08bbfe552824788e9dd
Leydson Andrey <andrey.tar.xz@gmail.com>
2024-11-20 11:40:50 -0300

    melhora o desempenho 2

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
 import { WebView } from 'react-native-webview';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, BackHandler } from 'react-native';
 
 export default function App() {
+  const webViewRef = useRef();
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const handleBackButtonPress = () => {
+    if (canGoBack && webViewRef.current) {
+      webViewRef.current.goBack();
+    } else {
+      BackHandler.exitApp();
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBackButtonPress);
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBackButtonPress);
+    };
+  }, [canGoBack]);
+
   return (
     <WebView
       style={styles.container}
       source={{ uri: 'https://retangulo.pages.dev' }}
+      ref={webViewRef}
       cacheEnabled={true}
       thirdPartyCookiesEnabled
       sharedCookiesEnabled
@@ -13,6 +34,10 @@ export default function App() {
       automaticallyAdjustContentInsets={true}
       javaScriptEnabled={true}
       androidLayerType="hardware"
+      scrollEnabled
+      setBuiltInZoomControls={false}
+      domStorageEnabled={true} 
+      onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
     />
   );
 }
